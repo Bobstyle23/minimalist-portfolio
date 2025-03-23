f5dbbf335726b6540b6d8dfee20d47d7f875c9ca
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-03-23 13:00:54 +0900

    Complete navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
+const openMenu = document.querySelector(".navigation__menu-btn");
+const closeMenu = document.querySelector(".navigation__menu-btn--close");
+const navigation = document.querySelector(".navigation");
+const navigationListItems = navigation.childNodes[1].children;
 
+const toggleMenu = (topValue) => {
+  navigation.style.top = topValue;
+  openMenu.classList.toggle("hidden");
+  closeMenu.classList.toggle("hidden");
+};
+
+closeMenu.addEventListener("click", () => toggleMenu("-100%"));
+
+openMenu.addEventListener("click", () => toggleMenu("8.8rem"));
+
+for (let item of navigationListItems) {
+  item.addEventListener("click", () => {
+    setTimeout(() => {
+      toggleMenu("-100%");
+    }, 200);
+  });
+}
